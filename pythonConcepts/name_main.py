3331fda807f0e321f510fbc3332a79da4df66ad0
avinash mishra <avi@avinashs-MacBook-Pro.local>
2018-02-07 17:13:03 +0900

    main vs init printing code

---
+++ pythonConcepts/name_main.py
@@ -0,0 +1,16 @@
+
+class NameMain:
+
+    def __init__(self):
+        print("avi")
+
+    if __name__ == "__main__":
+        print("hi")
+    else:
+        print("bye")
+
+
+test = NameMain()
+'''
+o/p : hi avi
+'''
